244c6156c0a5063cf7a4447cd60c7739f8334267
elina <elina.kalnina@gmail.com>
2024-09-10 10:46:40 +0300

    Add project files.

---
+++ Lekcija2_2024/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Lekcija2_2024;
+
+Console.WriteLine("Hello, World!");
+Console.WriteLine(TestMethodOverload.transformtotext(true));
+Console.WriteLine(TestMethodOverload.transformtotext(5));
+Console.WriteLine(TestMethodOverload.transformtotext(false, 3));
---
+++ Lekcija2_2024/TestMethodOverload.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Lekcija2_2024
+{
+        public static class TestMethodOverload
+        {
+            public static string transformtotext(int i)
+            {
+                Debug.Print("int version");
+                return i.ToString();
+            }
+
+            public static string transformtotext(bool b)
+            {
+                Debug.Print("bool version");
+                return b.ToString();
+            }
+
+            public static string transformtotext(bool b, int i)
+            {
+                Debug.Print("bool and int version");
+                return b.ToString();
+            }
+        }
+    }
